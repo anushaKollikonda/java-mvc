6bb825d517e9c599f7a89023f00f44b86669cf70
anushakollikonda <kollikonda.anusha@gmail.com>
2023-08-09 15:51:58 +0530

    v3

---
+++ src/main/java/com/org/mvc/Bmi.java
@@ -0,0 +1,31 @@
+package com.org.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+class Bmi {
+
+    @GetMapping("/calculate-bmi")
+    public String calculateBMI(@RequestParam double weight, @RequestParam double height) {
+        double bmi = weight / (height * height);
+
+        String feedback;
+        if (bmi < 18.5) {
+            feedback = "You are underweight. Consider increasing your weight.";
+        } else if (bmi >= 18.5 && bmi < 24.9) {
+            feedback = "Your BMI is in the healthy range. Keep up the good work!";
+        } else if (bmi >= 25 && bmi < 29.9) {
+            feedback = "You are overweight. Consider decreasing your weight.";
+        } else {
+            feedback = "Your BMI is very high. It's recommended to consult a healthcare professional.";
+        }
+
+        return "Your BMI: " + bmi + "\nFeedback: " + feedback;
+    }
+}
